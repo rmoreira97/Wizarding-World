31d454a092c1b5b7b32d7995c46db63ef02cdae1
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-06 17:31:39 -0400

    includes chatbot,wheel, confetti that coressponds with house colors, css styling, banner

--- index.js
+++ index.js
@@ -4,15 +4,48 @@ const studentName = document.getElementById("student-name");
 const studentGender = document.getElementById("student-gender");
 const studentHouse = document.getElementById("student-house");
 const studentDateOfBirth = document.getElementById("student-dateofbirth");
-const pics = document.querySelector("#student-pic");
+const studentImage = document.querySelector("#student-pic");
 const form = document.querySelector("#new-form");
-
 const newName = document.querySelector("#name");
 const newGender = document.querySelector("#gender");
 const newHouse = document.querySelector("#house");
 const newDob = document.querySelector("#dob");
 const newImage = document.querySelector("#image-input");
+const wheelIframe = document.getElementById("wheel-iframe");
+const houseInput = document.getElementById("house");
 
+
+const jsConfetti = new JSConfetti();
+
+function triggerConfetti() {
+  jsConfetti.addConfetti({
+    confettiColors: getConfettiColors(newHouse.value), // Get the confetti colors based on the house
+  });
+}
+
+function getConfettiColors(house) {
+  switch (house.toLowerCase()) {
+    case 'gryffindor':
+      console.log('Selected house: Gryffindor');
+      return ['#FF0000', '#FFD700']; // Red and Gold
+    case 'slytherin':
+      console.log('Selected house: Slytherin');
+      return ['#228B22', '#000000']; // Green and Black
+    case 'hufflepuff':
+      console.log('Selected house: Hufflepuff');
+      return ['#0000FF', '#FFD700']; // Blue and Gold
+    case 'ravenclaw':
+      console.log('Selected house: Ravenclaw');
+      return ['#0000FF', '#000000']; // Blue and Black
+    default:
+      console.log('Selected house: Default');
+      return ['#FFFFFF', '#FFFFFF']; // Default to white
+  }
+}
+
+
+
+// Fetch data and set up event listeners here...
 fetch("http://localhost:3000/students")
   .then((res) => res.json())
   .then((data) => {
@@ -28,53 +61,71 @@ fetch("http://localhost:3000/students")
     });
     addStudents(data[0]);
   });
-
-function addStudents(student) {
-  pics.src = student.image;
-  studentName.textContent = student.name;
-  studentGender.textContent = student.gender;
-  studentHouse.textContent = student.house;
-  studentDateOfBirth.textContent = student.dateOfBirth;
-}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const imgTwo = document.createElement("img");
-  imgTwo.src = newImage.value;
-  imgTwo.id = "imgTwo";
-  galleryDiv.append(imgTwo);
-
-  imgTwo.addEventListener("click", (e) => {
-    addStudents(newStudent);
-  });
-
-  const newStudent = {};
-  newStudent["name"] = newName.value;
-  newStudent["gender"] = newGender.value;
-  newStudent["house"] = newHouse.value;
-  newStudent["dateOfBirth"] = newDob.value;
-  newStudent["image"] = newImage.value;
-
-  addStudents(newStudent);
-});
-
-// Get references to the iframe and the "house" input field
-const wheelIframe = document.getElementById("wheel-iframe");
-const houseInput = document.getElementById("house");
+  
+  function addStudents(student) {
+    studentImage.src = student.image;
+    studentName.textContent = student.name;
+    studentGender.textContent = student.gender;
+    studentHouse.textContent = student.house;
+    studentDateOfBirth.textContent = student.dateOfBirth;
+  }
+  
 
 window.addEventListener("message", (event) => {
   // Check if the message is from the Wheel Decide iframe
   if (event.source === wheelIframe.contentWindow) {
     console.log("Received data:", event.data);
-    
-    // Extract and display the selected value
-    const selectedValue = event.data.split('-')[1];
-    console.log("Extracted value:", selectedValue);
 
+    // Extract and display the selected value
+    const selectedValue = event.data.split('-')[1].trim().toLowerCase();;
+    console.log("Extracted value:", selectedValue);
+    // capitalize 
+    function capitalizeWords(str) {
+      return str.replace(/\b\w/g, (char) => char.toUpperCase());
+    }
     // Set the selected value in the "house" input field
-    houseInput.value = selectedValue;
+    houseInput.value = capitalizeWords(selectedValue);
   }
 });
-  
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (
+    newName.value.trim() === "" ||
+    newGender.value.trim() === "" ||
+    newHouse.value.trim() === "" ||
+    newDob.value.trim() === "" ||
+    newImage.value.trim() === ""
+  ) {
+    alert("Please fill out all fields before adding a new student.");
+    return;
+  }
+
+  const house = newHouse.value; // Get the selected house from the form
+
+  const imgTwo = document.createElement("img");
+  imgTwo.src = newImage.value;
+  imgTwo.id = "sImage";
+  galleryDiv.appendChild(imgTwo);
+  imgTwo.addEventListener("click", (e) => {
+    addStudents(newStudent);
+  });
+
+  const newStudent = {
+    name: newName.value,
+    gender: newGender.value,
+    house: house,
+    dateOfBirth: newDob.value,
+    image: newImage.value,
+  };
+
+  // Trigger the confetti animation with the selected house color
+  triggerConfetti(house);
+
+  // Clear the form
+  form.reset();
+
+  // Add the new student
+  addStudents(newStudent);
+});
